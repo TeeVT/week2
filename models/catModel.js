f8533bb1c9569e3e548b32cd1fdb96c105c45a53
TeeVT <93247900+TeeVT@users.noreply.github.com>
2021-11-10 10:04:21 +0200

    db redi

--- models/catModel.js
+++ models/catModel.js
@@ -26,7 +26,8 @@ const getCat = async (id, next) => {
     next(httpError('Database error', 500));
   }
 };
-const addCat = async (name, weight, owner, filename, birthdate, next ) => {
+
+const addCat = async (name, weight, owner, birthdate, filename, next) => {
   try {
     const [rows] = await promisePool.execute(
       'INSERT INTO wop_cat (name, weight, owner, filename, birthdate) VALUES (?, ?, ?, ?, ?)',
@@ -38,8 +39,37 @@ const addCat = async (name, weight, owner, filename, birthdate, next ) => {
     next(httpError('Database error', 500));
   }
 };
+
+const modifyCat = async (name, weight, owner, birthdate, cat_id, next) => {
+  try {
+    const [rows] = await promisePool.execute(
+      'UPDATE wop_cat SET name = ?, weight = ?, owner = ?, birthdate = ? WHERE cat_id = ?;',
+      [name, weight, owner, birthdate, cat_id]
+    );
+    return rows;
+  } catch (e) {
+    console.error('addCat error', e.message);
+    next(httpError('Database error', 500));
+  }
+};
+
+const deleteCat = async (id, next) => {
+  try {
+    const [rows] = await promisePool.execute(
+      'DELETE FROM wop_cat WHERE cat_id = ?',
+      [id]
+    );
+    return rows;
+  } catch (e) {
+    console.error('deleteCat error', e.message);
+    next(httpError('Database error', 500));
+  }
+};
+
 module.exports = {
   getAllCats,
   getCat,
   addCat,
+  modifyCat,
+  deleteCat,
 };
